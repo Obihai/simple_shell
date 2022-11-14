e3386878f1e15548bc12cabde30e71f97be8699e
Kaegan Davis Haidome <obichi@coseo.com>
2022-11-14 06:41:01 +0100

    Update lists1.c

--- lists1.c
+++ lists1.c
@@ -6,10 +6,10 @@
  *
  * Return: size of list
  */
-
 size_t list_len(const list_t *h)
 {
-	size_t i = 0;	
+	size_t i = 0;
+
 	while (h)
 	{
 		h = h->next;
@@ -24,7 +24,6 @@ size_t list_len(const list_t *h)
  *
  * Return: array of strings
  */
-
 char **list_to_strings(list_t *head)
 {
 	list_t *node = head;
@@ -47,6 +46,7 @@ char **list_to_strings(list_t *head)
 			free(strs);
 			return (NULL);
 		}
+
 		str = _strcpy(str, node->str);
 		strs[i] = str;
 	}
@@ -54,16 +54,17 @@ char **list_to_strings(list_t *head)
 	return (strs);
 }
 
+
 /**
  * print_list - prints all elements of a list_t linked list
  * @h: pointer to first node
  *
  * Return: size of list
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;	
+	size_t i = 0;
+
 	while (h)
 	{
 		_puts(convert_number(h->num, 10, 0));
@@ -79,14 +80,12 @@ size_t print_list(const list_t *h)
 
 /**
  * node_starts_with - returns node whose string starts with prefix
- *
  * @node: pointer to list head
  * @prefix: string to match
  * @c: the next character after prefix to match
  *
  * Return: match node or null
  */
-
 list_t *node_starts_with(list_t *node, char *prefix, char c)
 {
 	char *p = NULL;
@@ -108,27 +107,16 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
  *
  * Return: index of node or -1
  */
-
 ssize_t get_node_index(list_t *head, list_t *node)
-
 {
-
 	size_t i = 0;
 
 	while (head)
-
 	{
-
 		if (head == node)
-
 			return (i);
-
 		head = head->next;
-
 		i++;
-
 	}
-
 	return (-1);
-
 }
