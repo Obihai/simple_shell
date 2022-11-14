b99c8fe432e3c75c6d9eb931adf1dc8e8c9b3b16
Kaegan Davis Haidome <obichi@coseo.com>
2022-11-14 06:35:35 +0100

    Update lists1.c

--- lists1.c
+++ lists1.c
@@ -10,6 +10,7 @@
 size_t list_len(const list_t *h)
 {
 	size_t i = 0;
+	
 	while (h)
 	{
 		h = h->next;
@@ -64,6 +65,7 @@ char **list_to_strings(list_t *head)
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
+	
 	while (h)
 	{
 		_puts(convert_number(h->num, 10, 0));
