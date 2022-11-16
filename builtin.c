d1665b4e3384b2547e6303eb077c1fa288e78d0f
Kaegan Davis Haidome <obichi@coseo.com>
2022-11-16 06:52:25 +0100

    builtin

--- builtin.c
+++ builtin.c
@@ -2,12 +2,11 @@
 
 /**
  * _myexit - exits the shell
- *
- * @info: Structure containing potential arguments. Used to maintain constant function prototype.
- *
- * Return: exits with a given exit status(0) if info.argv[0] != "exit"
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
  */
-
 int _myexit(info_t *info)
 {
 	int exitcheck;
@@ -28,22 +27,20 @@ int _myexit(info_t *info)
 	}
 	info->err_num = -1;
 	return (-2);
-
 }
 
 /**
  * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain constant function prototype.
- *
- * Return: Always 0
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
  */
-
 int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
 	int chdir_ret;
-	s = getcwd(buffer, 1024);
 
+	s = getcwd(buffer, 1024);
 	if (!s)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!info->argv[1])
@@ -80,27 +77,21 @@ int _mycd(info_t *info)
 		_setenv(info, "PWD", getcwd(buffer, 1024));
 	}
 	return (0);
-
 }
 
 /**
  * _myhelp - changes the current directory of the process
- *
- * @info: Structure containing potential arguments. Used to maintain constant function prototype.
- *
- * Return: Always 0
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
  */
-
 int _myhelp(info_t *info)
 {
 	char **arg_array;
 
 	arg_array = info->argv;
 	_puts("help call works. Function not yet implemented \n");
-
 	if (0)
 		_puts(*arg_array); /* temp att_unused workaround */
-
 	return (0);
-
 }
