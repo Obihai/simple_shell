0e1b5cd478ab94c86cfe464841e3dfa3f8f9574d
Kaegan Davis Haidome <obichi@coseo.com>
2022-11-16 07:27:31 +0100

    lists

--- lists.c
+++ lists.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * add_node - adds a node to the start of the list
  * @head: address of pointer to head node
@@ -31,6 +32,7 @@ list_t *add_node(list_t **head, const char *str, int num)
 	*head = new_head;
 	return (new_head);
 }
+
 /**
  * add_node_end - adds a node to the end of the list
  * @head: address of pointer to head node
@@ -71,6 +73,8 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 		*head = new_node;
 	return (new_node);
 }
+
+
 /**
  * print_list_str - prints only the str element of a list_t linked list
  * @h: pointer to first node
@@ -90,6 +94,7 @@ size_t print_list_str(const list_t *h)
 	}
 	return (i);
 }
+
 /**
  * delete_node_at_index - deletes node at given index
  * @head: address of pointer to first node
@@ -129,9 +134,11 @@ int delete_node_at_index(list_t **head, unsigned int index)
 	}
 	return (0);
 }
+
 /**
  * free_list - frees all nodes of a list
  * @head_ptr: address of pointer to head node
+ *
  * Return: void
  */
 void free_list(list_t **head_ptr)
